33646a4068dbfbeadf088fc3c3cc68c989627873
coreyan1234 <an.co@northeastern.edu>
2024-02-20 21:20:24 -0500

    Tried implementing the chevron nav, but it needs to be reworked

--- src/Kanbas/Courses/Home/index.tsx
+++ src/Kanbas/Courses/Home/index.tsx
@@ -1,6 +1,8 @@
 import { FaBars, FaBell, FaBullhorn, FaChartArea, FaChartBar, FaChevronDown, FaCrosshairs, FaFileImport, FaGlasses } from "react-icons/fa";
 import ModuleList from "../Modules/List";
 import "./index.css";
+import ChevronNav from "../Navigation/chevron_nav";
+import { Link } from "react-router-dom";
 
 function Home() {
     const buttons = [
@@ -34,43 +36,47 @@ function Home() {
         }
     ]
 
-  return (
-    <div>
-        <div className="d-block d-sm-block d-md-none">
-            {/* Hamburger + Chevron */}
-            <div className="d-flex justify-content-between bg-black" style={{alignItems: "center"}}>
-                <a href="/Kanbas/Navigation/index_hamburger.html"><FaBars className="text-white"/></a>
-                <div className="d-flex text-center text-white">
-                    CS4550.12631.202410
-                    <br />
-                    Modules
+    return (
+        <div>
+            <div className="d-block d-sm-block d-md-none">
+                {/* Hamburger + Chevron */}
+                <div className="d-flex justify-content-between bg-black" style={{alignItems: "center"}}>
+                    <a href="/Kanbas/Navigation/index_hamburger.html"><FaBars className="text-white"/></a>
+                    <div className="d-flex text-center text-white">
+                        CS4550.12631.202410
+                        <br />
+                        Modules
+                    </div>
+                    <Link to={<ChevronNav />} className="text-white" style={{float: "right"}}>
+                        <FaGlasses />
+                        <FaChevronDown />
+                    </Link>
+                    <a href="/Kanbas/Courses/Navigation/index_chevron.html" className="text-white">
+                        <FaGlasses />
+                        <FaChevronDown />
+                    </a>
                 </div>
-                <a href="/Kanbas/Courses/Navigation/index_chevron.html" className="text-white">
-                    <FaGlasses />
-                    <FaChevronDown />
-                </a>
             </div>
-        </div>
-        <div className="wd-flex-row-container">
-            <ModuleList />
             <div className="wd-flex-row-container">
-                <div className="flex-grow-0 me-2 d-none d-lg-block" style={{paddingLeft: "25px"}}>
-                    <h5>Course Status</h5>
-                    <div className="wd-flex-row-container">
-                        <button className="col unpublish-button"><i className="fa fa-ban"></i> Unpublish</button>
-                        <button className="col published-button"><i className="fa fa-circle-check"></i> Published</button>
-                    </div>
-                    <br />
-                    <div className="stacked-buttons">
-                        { buttons.map((button) => (
-                            <div><button className="button">{button.icon} {button.description}</button></div>
-                        ))}
+                <ModuleList />
+                <div className="wd-flex-row-container">
+                    <div className="flex-grow-0 me-2 d-none d-lg-block" style={{paddingLeft: "25px"}}>
+                        <h5>Course Status</h5>
+                        <div className="wd-flex-row-container">
+                            <button className="col unpublish-button"><i className="fa fa-ban"></i> Unpublish</button>
+                            <button className="col published-button"><i className="fa fa-circle-check"></i> Published</button>
+                        </div>
+                        <br />
+                        <div className="stacked-buttons">
+                            { buttons.map((button) => (
+                                <div><button className="button">{button.icon} {button.description}</button></div>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
+            
         </div>
-        
-    </div>
-  );
+    );
 }
 export default Home;
