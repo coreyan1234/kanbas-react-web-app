e78060f06f4ec2b7fb4c959d4cf05d1eb53df357
coreyan1234 <an.co@northeastern.edu>
2024-02-22 19:27:20 -0500

    Fixed the hamburger navigation links

--- src/Kanbas/Courses/Navigation/chevron_nav.tsx
+++ src/Kanbas/Courses/Navigation/chevron_nav.tsx
@@ -27,33 +27,26 @@ function ChevronNav() {
     const course = courses.find((course) => course._id === courseId);
     return(
         <div className="wd-flex-row-container">
-            <div>
-                <ul className="wd-navigation">
-                    <li><FaHome /><Link to={`/Kanbas/Courses/${course?._id}/`}> Home</Link></li>
-                    <li><FaSpinner /><Link to={`/Kanbas/Courses/${course?._id}/Modules`}> Modules</Link></li>
-                    <li><FaPlug /><Link to={`http://piazza.com`}> Piazza</Link></li>
-                    <li><FaPlug /><Link to={"#"}> Zoom Meetings</Link></li>
-                    <li><FaBriefcase /><Link to={`/Kanbas/Courses/${course?._id}/Assignments`}> Assignments</Link></li>
-                    <li><FaRocket /><Link to={"#"}> Quizzes</Link></li>
-                    <li><FaClipboardCheck /><Link to={`/Kanbas/Courses/${course?._id}/Grades`}> Grades</Link></li>
-                    <li><FaUsers /><Link to={"#"}> People</Link></li>
-                    <li><FaPlug /><Link to={"#"}> Panopto Video</Link></li>
-                    <li><FaComment /><Link to={"#"}> Discussions </Link><FaRegEyeSlash/></li>
-                    <li><FaBullhorn /><Link to={"#"}> Announcements </Link><FaRegEyeSlash/></li>
-                    <li><FaFile /><Link to={"#"}> Pages </Link><FaRegEyeSlash/></li>
-                    <li><FaFolder /><Link to={"#"}> Files </Link><FaRegEyeSlash/></li>
-                    <li><FaList /><Link to={"#"}> Rubrics </Link><FaRegEyeSlash/></li>
-                    <li><FaBullseye /><Link to={"#"}> Outcomes </Link><FaRegEyeSlash/></li>
-                    <li><FaRegCircle /><Link to={"#"}> Collaborations </Link><FaRegEyeSlash/></li>
-                    <li><FaBookOpen /><Link to={"#"}> Syllabus </Link><FaRegEyeSlash/></li>
-                    <li><FaCog /><Link to={"#"}> Settings</Link></li>
-                </ul>
-            </div>
-            <div className="exit-icon">
-                <Link to={`/Kanbas/Courses/${course?._id}/Home`} className="text-white" style={{float: "right"}}>
-                    <FaTimes/>
-                </Link>
-            </div>
+            <ul className="wd-navigation">
+                <li><FaHome /><Link to={`/Kanbas/Courses/${course?._id}/`}> Home</Link></li>
+                <li><FaSpinner /><Link to={`/Kanbas/Courses/${course?._id}/Modules`}> Modules</Link></li>
+                <li><FaPlug /><Link to={`http://piazza.com`}> Piazza</Link></li>
+                <li><FaPlug /><Link to={"#"}> Zoom Meetings</Link></li>
+                <li><FaBriefcase /><Link to={`/Kanbas/Courses/${course?._id}/Assignments`}> Assignments</Link></li>
+                <li><FaRocket /><Link to={"#"}> Quizzes</Link></li>
+                <li><FaClipboardCheck /><Link to={`/Kanbas/Courses/${course?._id}/Grades`}> Grades</Link></li>
+                <li><FaUsers /><Link to={"#"}> People</Link></li>
+                <li><FaPlug /><Link to={"#"}> Panopto Video</Link></li>
+                <li><FaComment /><Link to={"#"}> Discussions </Link><FaRegEyeSlash/></li>
+                <li><FaBullhorn /><Link to={"#"}> Announcements </Link><FaRegEyeSlash/></li>
+                <li><FaFile /><Link to={"#"}> Pages </Link><FaRegEyeSlash/></li>
+                <li><FaFolder /><Link to={"#"}> Files </Link><FaRegEyeSlash/></li>
+                <li><FaList /><Link to={"#"}> Rubrics </Link><FaRegEyeSlash/></li>
+                <li><FaBullseye /><Link to={"#"}> Outcomes </Link><FaRegEyeSlash/></li>
+                <li><FaRegCircle /><Link to={"#"}> Collaborations </Link><FaRegEyeSlash/></li>
+                <li><FaBookOpen /><Link to={"#"}> Syllabus </Link><FaRegEyeSlash/></li>
+                <li><FaCog /><Link to={"#"}> Settings</Link></li>
+            </ul>
         </div>
     )
 }
