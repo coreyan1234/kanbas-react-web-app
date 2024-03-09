837b3ef65a163e0658e92438502cc0fdae500c16
coreyan1234 <an.co@northeastern.edu>
2024-03-09 14:09:24 -0500

    Finished setting up lab routes

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,18 +1,19 @@
 import Assignment3 from "./a3";
 import Assignment4 from "./a4";
 import Nav from "../Nav";
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Navigate, Route, Routes } from "react-router-dom";
 
 function Labs() {
    return (
      <div className="container-fluid">
        <h1>Labs</h1>
        <Nav />
-       <Link to="/Labs/a3">Assignment 3</Link> |
-       <Link to="/Labs/a4">Assignment 4</Link>
+       {/* <Link to="/Labs/a3">Assignment 3</Link> |
+       <Link to="/Labs/a4">Assignment 4</Link> */}
        <Routes>
-         <Route path="/a3/*" element={<Assignment3 />} />
-         <Route path="/a4" element={<Assignment4 />} />
+        <Route path="/" element={<Navigate to="a3"/>} />
+         <Route path="/a3" element={<Assignment3 />} />
+         <Route path="a4" element={<Assignment4 />} />
        </Routes>
      </div>
    );  
