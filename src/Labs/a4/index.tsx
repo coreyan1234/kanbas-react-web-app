38618e46b51ea099435d6b002029160ebec4b382
coreyan1234 <an.co@northeastern.edu>
2024-03-10 22:46:11 -0400

    lab so far

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
 
 const Assignment4 = () => {
   return(
     <>
       <h1>Assignment 4</h1>
+      <ClickEvent />
+      <PassingDataOnEvent />
     </>
   );
 };
