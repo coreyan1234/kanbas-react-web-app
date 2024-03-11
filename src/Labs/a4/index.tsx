513ddcafa16994d7093f5e9a2e09ec712ca950b8
coreyan1234 <an.co@northeastern.edu>
2024-03-10 23:36:44 -0400

    Lab up to 2.3

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,13 +1,32 @@
 import React from "react";
 import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
 
 const Assignment4 = () => {
+  function sayHello() {
+    alert("Hello");
+  }
   return(
     <>
       <h1>Assignment 4</h1>
       <ClickEvent />
       <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject />
+      <Counter />
+      <BooleanStateVariables />
+      <DateStateVariable />
+      <ObjectStateVariable />
+      <ArrayStateVariable />
+      <ParentStateComponent />
     </>
   );
 };
